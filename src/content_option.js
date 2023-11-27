d7adc4f331b90593606ec00d25d96b6f9f6f26d1
Hadi <abdulhady961@gmail.com>
2023-11-27 09:32:19 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+const logotext = "A.H";
+const meta = {
+    title: "Abdul Hadi",
+    description: "I'm Abdul Hadi, a technologist and Developer.",
+};
+
+const introdata = {
+    title: "Hi, I'm Abdul Hadi",
+    animated: {
+        first: "Technologist",
+        second: "Visionary",
+        third: "Developer",
+        fourth: "Programmer",
+        fifth: "Debuger",
+        sixth: "Analyst",
+        // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+    },
+    description: "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+    your_img_url: "https://r4.wallpaperflare.com/wallpaper/614/208/24/iron-man-wallpaper-b4ef3e9e3a6eb0409dfc21c25125469f.jpg",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "I am Abdul Hadi An Software Developer Working On Project Using React,Node,HTML,Css",
+};
+const worktimeline = [{
+        jobtitle: "Example 1",
+        where: "Google",
+        date: "2023",
+    },
+    {
+        jobtitle: "Example 2",
+        where: "Micosoft",
+        date: "2022",
+    },
+    {
+        jobtitle: "Example 3",
+        where: "Meta",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+    },
+    
+    {
+        name: "C",
+    },
+    
+    {
+        name: "Java",
+    },
+    
+    {
+        name: "Bash",
+    },
+
+    {
+        name: "Powershell",
+    },
+
+    {
+        name: "Reverse Engineering",
+    },
+
+    {
+        name: "Web-Exploitation",
+    },
+
+    {
+        name: "Network Exploitation",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "test@test.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "SERVICE_ID",
+    YOUR_TEMPLATE_ID: "TEMPLATE_ID",
+    YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
+};
+
+const socialprofils = {
+    github: "https://github.com/",
+    linkedin: "https://linkedin.com/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
